c167cfefe3ee25fdc1c5251e7f16124c521c94f0
RafiMoreno <ahmadarrafi.m@gmail.com>
2024-07-03 19:37:12 +0700

    initialize spring template

---
+++ src/test/java/com/api/itsec_test/ItsecTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.itsec_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsecTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
